e4b11c1c05306f2731878f6595137cc0e9c3b333
benbencong <981658263@qq.com>
2020-05-21 00:27:56 +0800

    第二次作业

--- src/main/java/com/benben/firstUniqChar.java
+++ src/main/java/com/benben/firstUniqChar.java
@@ -10,6 +10,10 @@ public class firstUniqChar {
     }
     /*
     给定一个字符串，找到它的第一个不重复的字符，并返回它的索引。如果不存在，则返回 -1。
+    思路：
+    1、因为要找到第一个不重复的字符，用LinkedHashMap保证有序
+    2、遍历子符串，查看map中key是否包含该字符，包含的话就把该key对应的值+1，否则添加该字符为key到map中，值为1
+    3、遍历map，查看第一个值为1的key即为第一个不重复的字符，然后在用index在字符串中找到索引位置
      */
     public static int query(String s){
         HashMap<String,Integer> map1=new LinkedHashMap<>();
